b7d9c7bc893dea9658015a595c0a18ecb2a27047
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-09-29 11:58:48 -0400

    ui: add dash icon

--- Blasphemous.Modding.Installer/Properties/Resources.Designer.cs
+++ Blasphemous.Modding.Installer/Properties/Resources.Designer.cs
@@ -113,6 +113,16 @@ namespace Blasphemous.Modding.Installer.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icon_dash_light {
+            get {
+                object obj = ResourceManager.GetObject("icon-dash-light", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icon_x_light {
             get {
                 object obj = ResourceManager.GetObject("icon-x-light", resourceCulture);
