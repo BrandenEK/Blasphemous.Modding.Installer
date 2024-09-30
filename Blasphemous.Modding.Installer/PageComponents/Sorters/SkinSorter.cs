287280be5270b396bc916938c0e933694e74669e
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-09-30 10:02:34 -0400

    sorting: change functionality of sorters

--- Blasphemous.Modding.Installer/PageComponents/Sorters/SkinSorter.cs
+++ Blasphemous.Modding.Installer/PageComponents/Sorters/SkinSorter.cs
@@ -1,40 +1,24 @@
-﻿using Blasphemous.Modding.Installer.PageComponents.UIHolders;
-using Blasphemous.Modding.Installer.Skins;
+﻿using Blasphemous.Modding.Installer.Skins;
 
 namespace Blasphemous.Modding.Installer.PageComponents.Sorters;
 
-internal class SkinSorter : ISorter
+internal class SkinSorter : ISorter<Skin>
 {
-    private readonly IUIHolder _uiHolder;
-    private readonly List<Skin> _skins;
     private readonly SectionType _section;
 
-    public SkinSorter(IUIHolder uiHolder, List<Skin> skins, SectionType section)
+    public SkinSorter(SectionType section)
     {
-        _uiHolder = uiHolder;
-        _skins = skins;
         _section = section;
     }
 
-    public void Sort()
+    public IEnumerable<Skin> Sort(IEnumerable<Skin> list)
     {
         var comparer = new SkinPropertyComparer(Core.SettingsHandler.Properties.GetSort(_section));
 
-        var sorted = _skins
+        return list
             .OrderBy(mod => mod, comparer)
             .ThenBy(mod => mod.Data.name)
             .ToArray();
-
-        _skins.Clear();
-        _skins.AddRange(sorted);
-
-        _uiHolder.SectionPanel.VerticalScroll.Value = 0;
-
-        int idx = 0;
-        foreach (Skin skin in _skins)
-        {
-            skin.SetUIPosition(idx++);
-        }
     }
 
     class SkinPropertyComparer : IComparer<Skin>
