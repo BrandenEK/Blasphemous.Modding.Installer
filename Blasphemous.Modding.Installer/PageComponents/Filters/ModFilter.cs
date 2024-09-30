a928c7e2c3d269753baf41bc8ff55d3cebab6973
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-09-30 11:29:53 -0400

    filtering: add both filters

---
+++ Blasphemous.Modding.Installer/PageComponents/Filters/ModFilter.cs
@@ -0,0 +1,27 @@
+﻿using Blasphemous.Modding.Installer.Mods;
+
+namespace Blasphemous.Modding.Installer.PageComponents.Filters;
+
+internal class ModFilter : IFilter<Mod>
+{
+    private readonly SectionType _section;
+
+    public ModFilter(SectionType section)
+    {
+        _section = section;
+    }
+
+    public IEnumerable<Mod> Filter(IEnumerable<Mod> list)
+    {
+        FilterType filter = Core.SettingsHandler.Properties.GetFilter(_section);
+
+        return filter switch
+        {
+            FilterType.NotInstalled => list.Where(x => !x.Installed),
+            FilterType.Installed => list.Where(x => x.Installed),
+            FilterType.Disabled => list.Where(x => !x.Enabled),
+            FilterType.Enabled => list.Where(x => x.Enabled),
+            _ => list
+        };
+    }
+}
---
+++ Blasphemous.Modding.Installer/PageComponents/Filters/SkinFilter.cs
@@ -0,0 +1,25 @@
+﻿using Blasphemous.Modding.Installer.Skins;
+
+namespace Blasphemous.Modding.Installer.PageComponents.Filters;
+
+internal class SkinFilter : IFilter<Skin>
+{
+    private readonly SectionType _section;
+
+    public SkinFilter(SectionType section)
+    {
+        _section = section;
+    }
+
+    public IEnumerable<Skin> Filter(IEnumerable<Skin> list)
+    {
+        FilterType filter = Core.SettingsHandler.Properties.GetFilter(_section);
+
+        return filter switch
+        {
+            FilterType.NotInstalled => list.Where(x => !x.Installed),
+            FilterType.Installed => list.Where(x => x.Installed),
+            _ => list
+        };
+    }
+}
