64dd5a1234836957e187882b77a96e509f615fcb
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-09-30 20:35:12 -0400

    config: add bigger settings object

---
+++ Blasphemous.Modding.Installer/Config/InstallerSettings.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Blasphemous.Modding.Installer.Config;
+
+public class InstallerSettings
+{
+    public WindowSettings Window { get; set; } = new();
+
+    public SectionType LastSection { get; set; } = SectionType.Blas1Mods;
+
+    public List<GameSettings> Games { get; set; } = new();
+    public List<PageSettings> Pages { get; set; } = new();
+}
+
+public class WindowSettings
+{
+    public Point Location { get; set; } = new Point(0, 0);
+    public Size Size { get; set; } = new Size(1, 1);
+    public bool IsMaximized { get; set; } = true;
+}
+
+public class GameSettings
+{
+    public string Id { get; set; } = string.Empty;
+    public string RootFolder { get; set; } = string.Empty;
+    public LaunchOptions LaunchOptions { get; set; } = new LaunchOptions(true, true);
+}
+
+public class PageSettings
+{
+    public string Id { get; set; } = string.Empty;
+    public SortType Sort { get; set; } = SortType.Name;
+    public FilterType Filter { get; set; } = FilterType.All;
+    public DateTime Time { get; set; } = DateTime.Now;
+}
