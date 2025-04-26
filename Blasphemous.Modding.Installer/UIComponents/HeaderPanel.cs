05a8b5048ac44d5658f4433f14b499e5e26092b7
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-04-26 10:05:17 -0400

    ui: improve header placement code

--- Blasphemous.Modding.Installer/UIComponents/HeaderPanel.cs
+++ Blasphemous.Modding.Installer/UIComponents/HeaderPanel.cs
@@ -1,5 +1,4 @@
-﻿using Basalt.Framework.Logging;
-using Blasphemous.Modding.Installer.Models;
+﻿using Blasphemous.Modding.Installer.Models;
 
 namespace Blasphemous.Modding.Installer.UIComponents;
 
@@ -16,14 +15,13 @@ public class HeaderPanel : Panel
 
         if (!Core.Initialized)
             return;
-
-        Logger.Info("Repainting header!");
-        HeaderImage header = Core.CurrentPage.Header;
         
+        HeaderImage header = Core.CurrentPage.Header;
         var g = e.Graphics;
-        var position = new Point(header.FlipAlign ? Width - header.Image.Width : 0, header.Offset);
+        var location = new Point(header.FlipAlign ? Width - header.Image.Width : 0, header.Offset);
+        var size = new Size(header.Image.Width, header.Image.Height);
 
         g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
-        g.DrawImage(header.Image, position.X, header.Offset, header.Image.Width, header.Image.Height);
+        g.DrawImage(header.Image, new Rectangle(location, size));
     }
 }
